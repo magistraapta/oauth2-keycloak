50bc6f52b225e47fddaaee77e99826f07ed2d232
Magistra Apta <31767116+magistraapta@users.noreply.github.com>
2025-10-02 12:39:24 +0800

    fix some issue

--- src/main/java/com/auth/keycloak_auth/config/SecurityConfig.java
+++ src/main/java/com/auth/keycloak_auth/config/SecurityConfig.java
@@ -5,28 +5,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/").permitAll()
-                .anyRequest().authenticated()
-            )
-            .oauth2Login(oauth2 -> oauth2
-                .loginPage("/oauth2/authorization/keycloak")
-                .defaultSuccessUrl("/")
-            )
-            .logout(logout -> logout
-                .invalidateHttpSession(true)
-                .clearAuthentication(true)
-                .deleteCookies("JSESSIONID")
-                .logoutSuccessUrl("/")
-            )
-            .build();
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .oauth2Login(withDefaults())
+                .build();
     }
 }
--- src/main/java/com/auth/keycloak_auth/controllers/AppController.java
+++ src/main/java/com/auth/keycloak_auth/controllers/AppController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AppController {
+
     @GetMapping("/")
     public String publicEndpoint() {
         return "home";
